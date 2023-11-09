7186829e673ae9b76a2d911f90bf6231ddc17a92
Benefrancis <benefrancis@gmail.com>
2023-11-09 10:23:04 -0300

    Initial commit

---
+++ src/main/java/br/com/fiap/domain/entity/Movie.java
@@ -0,0 +1,15 @@
+package br.com.fiap.domain.entity;
+
+public class Movie {
+
+    private Long id;
+
+    private String title;
+
+    private boolean adult;
+
+    private  Genre  genre;
+
+    private String originalLanguage;
+
+}
